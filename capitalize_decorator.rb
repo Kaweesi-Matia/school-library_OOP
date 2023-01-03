fe482751806461150cc5f346197957312540b5dc
Kaweesi Matia <matiakaweesi@gmail.com>
2023-01-03 16:19:56 +0300

    modify capitalize_decorator.rb

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -2,6 +2,5 @@ require './base_decorator'
 class CapitalizeDecorator < BaseDecorator
   def correct_name
     @nameable.correct_name.capitalize
-    puts @nameable.correct_name.capitalize
   end
 end
