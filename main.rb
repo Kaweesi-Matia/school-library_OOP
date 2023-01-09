9b233687c5a2feab2f1c7a36328dc4f2c4bb69bd
Shuhad786 <shuhad.loofer@gmail.com>
2023-01-09 10:02:31 +0200

    Refactor Main and App classes

--- main.rb
+++ main.rb
@@ -1,45 +1,14 @@
 require './app'
-def main
-  books = []
-  rental_data = []
-  people = []
-
-  options(books, people, rental_data)
-end
-
-def display_all
-  puts 'Welcome to the school library'
-  puts ' Please choose a task  basing on the number '
-  puts "   1:  Show all books.
-           2:  Show all people.
-           3: Create a person
-           4: Create a rental
-           5: Create a book
-           6: List rented books to a person by ID
-           7: Exit"
-end
-
-def options(books, people, rental_data)
-  loop do
-    display_all
-    option = gets.chomp.to_i
-    case option
-    when 1
-      show_books(books)
-    when 2
-      show_people(people)
-    when 3
-      add_people(people)
-    when 4
-      add_rental(books, people, rental_data)
-    when 5
-      add_book(books)
-    when 6
-      show_rental_data(rental_data)
-    else
-      break
-    end
+class Main
+  def main
+    app = App.new
+    books = []
+    rental_data = []
+    people = []
+    app.options(books, people, rental_data)
   end
 end
 
-main
+main = Main.new
+
+main.main
